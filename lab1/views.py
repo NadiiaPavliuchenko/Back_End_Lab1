45b922772ac688df8054db301f906899a8bf626b
NadiiaPavliuchenko <n.pavljuchenko@gmail.com>
2022-10-14 17:05:08 +0300

    Endpoint one: creating a user

--- lab1/views.py
+++ lab1/views.py
@@ -1,6 +1,27 @@
+import uuid
 from lab1 import app
+from flask import jsonify, request
+
+USERS = [
+    {
+        "id": uuid.uuid4().hex,
+        "name": "Ann"
+    },
+    {
+        "id": uuid.uuid4().hex,
+        "name": "Andy"
+    }
+]
 
 
 @app.route("/")
 def default_page():
     return "Default page"
+
+
+@app.route("/newUser", methods=['POST'])
+def create_user():
+    new_user = request.get_json()
+    USERS.append(new_user)
+    print(USERS)
+    return jsonify(new_user)
